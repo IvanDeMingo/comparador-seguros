3ca02642f3123861df0d47745c221b85b8e95b95
IvanDeMingo <ivandemingo2309@gmail.com>
2018-01-02 17:13:34 +0100

    Introduction added

---
+++ app/src/main/java/pae/seguros/introduction/IntroActivity.java
@@ -0,0 +1,35 @@
+package pae.seguros.introduction;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+import pae.seguros.R;
+
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addSlide(IntroFragment.newInstance(R.layout.intro_slide_1));
+        addSlide(IntroFragment.newInstance(R.layout.intro_slide_2));
+        addSlide(IntroFragment.newInstance(R.layout.intro_slide_3));
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+
+        finish();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+
+        finish();
+    }
+}
